3abc0e552e6d71c8527b940bc7e581f4dfb41389
julien3641 <3641@holbertonschool.com>
2022-01-13 16:46:58 +0100

    update conditions

--- _pstr.c
+++ _pstr.c
@@ -17,13 +17,13 @@ void _pstr(stack_t **stack, unsigned int line_number)
 		printf("\n");
 	while (tmp)
 	{
-		if (tmp->n != 0 && (tmp->n > 0 && tmp->n <= 127))
-			{
-				printf("%c", tmp->n);
-				tmp = tmp->next;
-			}
-			else
-				break;
-			}
+		if (tmp->n != 0 || (tmp->n > 0 && tmp->n <= 127))
+		{
+			printf("%c", tmp->n);
+			tmp = tmp->next;
+		}
+		else
+			break;
+	}
 	printf("\n");
 }
