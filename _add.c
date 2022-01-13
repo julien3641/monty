098e49b4d6ab730ba590badf8cb84ef3eaf46a6d
julien3641 <3641@holbertonschool.com>
2022-01-13 10:03:18 +0100

    update monty.h and add _add and _sub

---
+++ _add.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+ * _add - function that adds the top two elements of the stack
+ * @stack:
+ * @line_number:
+ * Return:
+ */
+
+void _add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current_node, *tmp;
+	unsigned int number_node;
+	int i, result;
+	(void)line_number;
+
+	current_node = *stack;
+	number_node = 0;
+	for (i = 0; current_node != NULL; i++)
+	{
+		number_node++;
+		current_node = current_node->next;
+	}
+	if (number_node <= 1)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		result = ((*stack)->next->n) + ((*stack)->n);
+		(*stack)->next->n = result;
+		tmp = *stack;
+		*stack = (*stack)->next;
+		free(tmp);
+		(*stack)->prev = NULL;
+	}
+}
--- monty.h
+++ monty.h
@@ -69,7 +69,9 @@ extern current_line cline;
 		{"pop", _pop},\
 		{"swap", _swap},\
 		{"nop", _nop},\
-	        {NULL, NULL} \
+		{"add", _add},\
+		{"sub", _sub},\
+	        {NULL, NULL}			\
 	}
 
 /* Prototype process */
@@ -85,5 +87,6 @@ void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
-
+void _add(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
 #endif /* MONTY_H */
