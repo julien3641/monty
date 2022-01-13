b88b7c672d6d70bd8959d6e77759d7339c4269c5
Elodie Riou <elodie.riou@hotmail.com>
2022-01-13 22:07:33 +0100

    fix code

--- _rotl.c
+++ _rotl.c
@@ -11,7 +11,7 @@ void _rotl(stack_t **stack, unsigned int line_number)
 	stack_t *tmp, *move;
 	(void)line_number;
 
-	if (*stack != NULL)
+	if (*stack != NULL || (*stack)->next != NULL)
 	{
 		tmp = *stack;
 		move = *stack;
@@ -20,7 +20,7 @@ void _rotl(stack_t **stack, unsigned int line_number)
 			move = move->next;
 
 		*stack = (*stack)->next;
-                (*stack)->prev = move->next;
+                (*stack)->prev = NULL;
 		tmp->next = NULL;
 		tmp->prev = move;
 		move->next = tmp;
